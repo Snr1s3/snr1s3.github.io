c7a393be328f4b85fad3379a24ace99b4041c787
Alba Segura <m1ss.sunrse@gmail.com>
2026-01-29 16:09:12 +0100

    Add initial configuration and sitemap files
    - Created robots.txt to manage web crawler access.
    - Added sitemap-0.xml for individual page indexing.
    - Created sitemap.xml to reference the sitemap-0.xml.
    - Introduced requirements.txt to specify project dependencies.
    - Added rxconfig.py for Reflex app configuration.

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-    base: '/snr1s3.github.io/',
-    site: 'https://snr1s3.github.io/',
-});
