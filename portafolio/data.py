e9e6163a8037a08ce9464e184fd6d3561e4e6755
Alba Segura <m1ss.sunrse@gmail.com>
2026-01-30 11:46:22 +0100

    Refactor data handling in Data class to ensure proper serialization of nested objects and improve data integrity

--- portafolio/data.py
+++ portafolio/data.py
@@ -13,11 +13,15 @@ class Technology:
     def __init__(self, icon, name):
         self.icon = icon
         self.name = name
+    def to_dict(self):
+        return {"icon": self.icon, "name": self.name}
         
 class LearningT:
     def __init__(self, icon, name):
         self.icon = icon
         self.name = name
+    def to_dict(self):
+        return {"icon": self.icon, "name": self.name}
 
 
 class Info:
@@ -28,7 +32,7 @@ class Info:
         self.description = description
         self.date = date
         self.certificate = certificate
-        self.technologies = [Technology(**tech) for tech in technologies]
+        self.technologies = [tech.to_dict() if hasattr(tech, 'to_dict') else tech for tech in technologies]
         self.image = image
         self.url = url
         self.github = github
@@ -40,6 +44,8 @@ class Extra:
         self.title = title
         self.description = description
         self.url = url
+    def to_dict(self):
+        return {"image": self.image, "title": self.title, "description": self.description, "url": self.url}
 
 
 class Data:
@@ -70,12 +76,12 @@ class Data:
         self.location = location
         self.media = Media(**media)
         self.about = about
-        self.technologies = [Technology(**tech) for tech in technologies]
-        self.learningt = [LearningT(**tech) for tech in learningt]
-        self.experience = [Info(**info) for info in experience]
-        self.projects = [Info(**info) for info in projects]
-        self.training = [Info(**info) for info in training]
-        self.extras = [Extra(**info) for info in extras]
+        self.technologies = [Technology(**tech).to_dict() if isinstance(tech, dict) else tech.to_dict() for tech in technologies]
+        self.learningt = [LearningT(**tech).to_dict() if isinstance(tech, dict) else tech.to_dict() for tech in learningt]
+        self.experience = [Info(**info).__dict__ if isinstance(info, dict) else info.__dict__ for info in experience]
+        self.projects = [Info(**info).__dict__ if isinstance(info, dict) else info.__dict__ for info in projects]
+        self.training = [Info(**info).__dict__ if isinstance(info, dict) else info.__dict__ for info in training]
+        self.extras = [Extra(**info).to_dict() if isinstance(info, dict) else info.to_dict() for info in extras]
 
 
 with open("assets/data/data.json") as file:
